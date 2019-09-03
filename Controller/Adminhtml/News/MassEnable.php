ac8792e6b55686cc1d729c003bc4f01bbebc5c65
DDEV-Local User <nobody@example.com>
2019-09-03 19:08:15 +0000

    [FEATURE] Admin grid - mass-actions

---
+++ Controller/Adminhtml/News/MassEnable.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+
+namespace Piuga\News\Controller\Adminhtml\News;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Ui\Component\MassAction\Filter;
+use Piuga\News\Model\ResourceModel\News\CollectionFactory;
+use Piuga\News\Model\News;
+
+/**
+ * Class MassEnable
+ * @package Piuga\News\Controller\Adminhtml\News
+ */
+class MassEnable extends Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Piuga_News::news';
+
+    /**
+     * @var Filter
+     */
+    protected $filter;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+
+    /**
+     * MassEnable constructor.
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(
+        Context $context,
+        Filter $filter,
+        CollectionFactory $collectionFactory
+    ) {
+        parent::__construct($context);
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    /**
+     * Execute enable mass-action
+     *
+     * @return ResultInterface
+     * @throws \Magento\Framework\Exception\LocalizedException|\Exception
+     */
+    public function execute() : ResultInterface
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        foreach ($collection as $item) {
+            $item->setStatus(News::STATUS_ENABLED);
+            $item->save();
+        }
+        $this->messageManager->addSuccessMessage(__('A total of %1 item(s) were enabled.', $collection->getSize()));
+
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+
+        return $resultRedirect->setPath('*/*/');
+    }
+}
